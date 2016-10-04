8bcd284a1b9c324d73ee1d5a29cc11adc70353e5
ImkeB <imke.botha@connect.qut.edu.au>
2016-10-04 13:40:56 +1000

    Added some code

--- OnTheSpot_Revised/OnTheSpot/Models/Package.cs
+++ OnTheSpot_Revised/OnTheSpot/Models/Package.cs
@@ -24,16 +24,12 @@ namespace OnTheSpot.Models
         public int PackageID { get; set; }
         public int OrderID { get; set; }
         public Status? Status { get; set; }
-        public string PickupAddress { get; set; }
-        public string DeliveryAddress { get; set; }
         public Priority? Priority { get; set; }
         public float Weight { get; set; }
         public DateTime? Collected { get; set; }
-        //public DateTime AtWarehouse { get; set; }
-        //public DateTime InTransit { get; set; }
         public DateTime? Delivered { get; set; }
-        //public bool Signature { get; set; }
         public string AssignedCourier { get; set; }
+        
 
         public virtual Order Order { get; set; }
         public virtual UserProfile UserProfile { get; set; }
