99a4b24d50900e8d8e7bc871db0f99ed24cdf3c4
victoria-tobin <victoria.tobin@connect.qut.edu.au>
2016-10-16 22:11:23 +1000

    Small CSS changes
    Did a bit of playing around with CSS and looking at getting the images
    in place of the buttons

---
+++ Personal_Portfolios/Victoria/Release2/Vehicles/VehiclesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnTheSpot.Models;
+using OnTheSpot.DAL;
+
+namespace OnTheSpot.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private DatabaseModels db = new DatabaseModels();
+
+        //
+        // GET: /Vehicles/
+
+        public ActionResult Index()
+        {
+            return View(db.Vehicles.ToList());
+        }
+
+        //
+        // GET: /Vehicles/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Vehicles/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vehicles.Add(vehicle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // POST: /Vehicles/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vehicle);
+        }
+
+        //
+        // GET: /Vehicles/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            if (vehicle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicle);
+        }
+
+        //
+        // POST: /Vehicles/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Vehicle vehicle = db.Vehicles.Find(id);
+            db.Vehicles.Remove(vehicle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
